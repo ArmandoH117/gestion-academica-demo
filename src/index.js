fcb2fd82d7e5b7c65f5e62cdc0a605482eacb66c
Jennifer Guerra <jennifer.guerra@ejemplo.com>
2025-09-19 22:15:33 -0400

    feat(dashboard): log inicial

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('hola');
+<<<<<<< HEAD
+=======
+console.log('rename/modify demo');
+console.log('dashboard listo');
+>>>>>>> 51b28d8 (feat(dashboard): log inicial)
+console.log('dashboard listo');
